af82e08930f65a86fe25fe7c61c072ba082c7695
Pierre-Hugues Husson <phh@phh.me>
2022-11-12 17:27:26 -0500

    No more "squeak" td it is, and it starts with t

--- build.sh
+++ build.sh
@@ -48,8 +48,8 @@ cp patches.zip release/$rom_fp/patches-for-developers.zip
     git clone https://github.com/phhusson/vendor_vndk -b android-10.0
 )
 
-buildVariant treble_arm64_bvS-userdebug squeak-arm64-ab-vanilla
-( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-squeak-arm64-ab-vndklite-vanilla.img.xz )
+buildVariant treble_arm64_bvS-userdebug td-arm64-ab-vanilla
+( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-td-arm64-ab-vndklite-vanilla.img.xz )
 
-buildVariant treble_a64_bvS-userdebug squeak-arm32_binder64-ab-vanilla
-( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-squeak-arm32_binder64-ab-vndklite-vanilla.img.xz )
+buildVariant treble_a64_bvS-userdebug td-arm32_binder64-ab-vanilla
+( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-td-arm32_binder64-ab-vndklite-vanilla.img.xz )
